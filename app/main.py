beef5dd6da8c3d0d1f63febe17f35a168f23305e
Fábia <fabiasiqueira13@gmail.com>
2026-06-12 01:04:32 +0000

    chore: base da aplicacao

---
+++ app/main.py
@@ -0,0 +1,11 @@
+import os
+
+def somar(a, b):
+    return a + b
+
+def conectar_api():
+    # Simulando o consumo de um Secret injetado em memoria
+    token = os.environ.get("MINHA_API_SECRET")
+    if not token:
+        raise ValueError("ERRO DE SEGURANCA: Token nao encontrado!")
+    return "Conexão Estabelecida com Sucesso!"
