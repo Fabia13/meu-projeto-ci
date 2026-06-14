faa729fb14b223f9ffec67ed31d7458f0e97ec99
Fábia <fabiasiqueira13@gmail.com>
2026-06-14 22:53:14 +0000

    chore: base da aplicacao

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 import os
 
+
 def somar(a, b):
     return a + b
 
+
 def conectar_api():
-    # Simulando o consumo de um Secret injetado em memoria
     token = os.environ.get("MINHA_API_SECRET")
     if not token:
-        raise ValueError("ERRO DE SEGURANCA: Token nao encontrado!")
+        raise ValueError("ERRO DE SEGURANÇA: Token não encontrado!")
     return "Conexão Estabelecida com Sucesso!"
-# Teste de Pull Request
